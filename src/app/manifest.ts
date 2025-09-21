8954500ecbd9da2771706d35697eae0cf77bbc8a
hews <hieudm.7998@gmail.com>
2025-09-21 21:53:40 +0700

    fix pwa

---
+++ src/app/manifest.ts
@@ -0,0 +1,46 @@
+// app/manifest.ts
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Minh Khôi Tracker',
+    short_name: 'BabyTracker',
+    description: 'Ứng dụng theo dõi bé',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon.ico',
+        sizes: '48x48', // ICO thường chứa nhiều kích thước
+        type: 'image/x-icon',
+      },
+    ],
+  };
+}
